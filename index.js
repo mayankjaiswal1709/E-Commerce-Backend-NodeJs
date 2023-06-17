5bf5e0272d7588df45b63777d9c11a68a9772738
Mayank Jaiswal <58311460+mayankjaiswal1709@users.noreply.github.com>
2023-06-17 21:48:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ require("./modles/config")
 const mainroutes = require('./routes/mainRoute')
 const app = express();
 require('colors')
-const Razorpay = require('razorpay');
+// const Razorpay = require('razorpay');
 
 app.use(express.json());
 app.use('/', mainroutes)
 
 
-const instance = new Razorpay({
-    key_id: process.env.RAZORPAY_API_KEY,
-    key_secret: process.env.RAZORPAY_API_SECRET,
-});
+// const instance = new Razorpay({
+//     key_id: process.env.RAZORPAY_API_KEY,
+//     key_secret: process.env.RAZORPAY_API_SECRET,
+// });
 
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`server running on Port no ${process.env.PORT}`)
 })
-module.exports = { server, instance }
+module.exports = { server} //instance }
